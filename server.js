efb2ec1f195887f3f30d029262fc02b127e68fa5
ToniSrt <4antoniogabriel20@gmail.com>
2022-01-28 18:50:24 -0300

    fixed typo

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-const Contenedor = require('./Contenedor')
+const Contenedor = require('./contenedor')
 const express = require('express')
 const app = express()
 
 const cont = new Contenedor("productos");
 
-app.get("/productos", (req, res) => {
-    res.send(`${cont.getAll()}`)
-    
-    res.end();
+app.get("/productos", async (req, res) => {
+    const productos = await cont.getAll()
+    res.json(productos)
 })
 
-
+app.get("/productoRandom", async (req, res) => {
+    const productos = await cont.getAll()
+    const pr = productos[Math.floor(Math.random() * productos.length)]
+    res.json(pr)
+})
 
 
 
